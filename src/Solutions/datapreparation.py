70c88e6f2ca7514561d1a6787a31b5d89193e201
Jath23 <jathushan2004@gmail.com>
2025-10-10 20:38:55 +0100

    +

--- src/Solutions/datapreparation.py
+++ src/Solutions/datapreparation.py
@@ -6,7 +6,13 @@ import matplotlib as plt
 # e.g. Summer = summer and Winter = winter
 def standardise_categories(df: pd.DataFrame, Col_Name):
     """
-    
+    This function changes similar categories into one of the same name
+
+    Parameters:
+    df, the dataframe and the column heading where the categories are located
+
+    Returns:
+    The number of data for each standardised category
     """
     # Locate the exact datapoints in the 'type' column, where the rows contain 'Summer'
     # Map them to 'summer'
@@ -15,6 +21,23 @@ def standardise_categories(df: pd.DataFrame, Col_Name):
 
     print(df[Col_Name].unique())
     print(df[Col_Name].value_counts())
+
+# New function that removes columns
+def remove_columns(df: pd.DataFrame, columns_tobe_removed):
+    """
+    Function removes the specified columns listed within the main function
+    and produces a new dataframe
+
+    Parameters:
+    df, the dataframe
+    columns_tobe_removed: the list of column names to be dropped
+
+    Returns:
+    The new dataframe with the specified columns removed
+    """
+
+    df_removed = df.drop(columns = columns_tobe_removed)
+    return df_removed
 
 
 
@@ -26,4 +49,10 @@ if __name__ == "__main__":
     csv_file = src_folder.joinpath('activities','data', 'paralympics_raw.csv')
     paralympics_df = pd.read_csv(csv_file)
 
+    # Standardising categories
     standardise_categories(paralympics_df, 'type')
+
+    # New dataframe with removed columns
+    columns_tobe_removed = ['URL','disabilities_included','highlights']
+    df_prepared = remove_columns(paralympics_df, columns_tobe_removed)
+    print(df_prepared)
